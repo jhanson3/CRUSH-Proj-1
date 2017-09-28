2b8b2ec73fa570e048ff3c783b5fc8e9ed1de9d1
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-09-28 14:51:24 -0700

    Added header comments to Gui and SimpleMath and set up main()

---
+++ src/controller/SimpleMath.java
@@ -0,0 +1,27 @@
+/*
+ * SimpleMath.java
+ * Author: Jeremiah Hanson
+ * ---------------------------------------------------------------------------
+ * Purpose: This project will get students to create new methods that will
+ * do certain simple geometric calculations when given certain parameters
+ * and check that their answer is correct. A gui will be used to help give the 
+ * students a more visual representation.
+ * 
+ * Responsibilities: This class is the controler for the whole project. It is 
+ * responsible for maintaining the gui.
+ */
+package controller;
+
+import view.Gui;
+
+public class SimpleMath {
+	
+	static Gui gui;
+
+	public static void main(String[] args) {
+		
+		gui = new Gui();
+		gui.setVisible(true);
+	}
+
+}
--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,5 +1,20 @@
+/*
+ * Gui.java
+ * Author: Jeremiah Hanson
+ * ---------------------------------------------------
+ * This class creates a gui interface for the students
+ * to input numbers into and buttons to choose what 
+ * equations to perform.
+ */
+
 package view;
 
-public class Gui {
+import javax.swing.JFrame;
+
+public class Gui extends JFrame{
+	
+	public Gui() {
+		
+	}
 
 }
