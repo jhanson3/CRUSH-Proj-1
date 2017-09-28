489323088037bc4c034dbf61013e41e26af05edf
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-09-28 15:15:28 -0700

    added code to gui to set up a button and get ready to accept a view

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -9,12 +9,37 @@
 
 package view;
 
+import java.awt.Container;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Gui extends JFrame{
 	
+	private static final long serialVersionUID = 4938045293212738100L;
+
+	private JPanel currView;
+	private Container cp;
+	private JButton triangle;
+	
 	public Gui() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1000, 1000);
+		this.setLocation(100, 40);
+		this.setTitle("Simple Math");
+		this.setLayout(null);
+		cp = new Container();
 		
+		initializeViewButtons();
+	}
+
+	private void initializeViewButtons() {
+		triangle = new JButton("Triangle");
+		triangle.setEnabled(false);
+		triangle.setLocation(10, 10);
+		triangle.setSize(200, 30);
+		this.add(triangle);
 	}
 
 }
